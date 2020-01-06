d013d04505bb932950f64c8387e2802d4bca9ddf
Cesar GONZALEZ <cesar.gonzalez@consulting-for.edenred.com>
2020-01-06 12:36:52 -0600

    Segundo commit

--- Program.cs
+++ Program.cs
@@ -16,16 +16,16 @@ namespace AsciiDecoding
 
         private static void Example()
         {
-            var asciiCode = "076 097 032 112 097 122 032 110 117 110 099 097 032 102 117 101 032 117 110 097 032 111 112 099 105 195 179 110";
-            Console.WriteLine( string.Format("Ascii code: {0}",asciiCode));
-
             //Mensaje ASCII = La paz nunca fue una opción
+            var asciiCode = "076 097 032 112 097 122 032 110 117 110 099 097 032 102 117 101 032 117 110 097 032 111 112 099 105 195 179 110";
+
+            Console.WriteLine( string.Format("Ascii code: {0}",asciiCode));
 
             //Convertir string a array de bytes con expresion lambda
             //1 Hacer split para obtener cada byte en string
             //2 Convertir strig a byte
             //3 Crear array
-            var asciiBytes = asciiCode.Split(' ').Select(v => Convert.ToByte(v)).ToArray();
+            var asciiBytes = asciiCode.Split(' ').Select(valor => Convert.ToByte(valor)).ToArray();
 
             //Convertir bytes a string con ascci
             var asciiMessage = Encoding.ASCII.GetString(asciiBytes);
