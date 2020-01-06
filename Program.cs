d55c20cda36077b046a9d1efbc71d8aba4d6538e
Cesar GONZALEZ <cesar.gonzalez@consulting-for.edenred.com>
2020-01-06 13:04:13 -0600

    Se agrega split de cadena, para obtener array de bytes

--- Program.cs
+++ Program.cs
@@ -17,7 +17,8 @@ namespace AsciiDecoding
         private static void Example()
         {
             //Mensaje ASCII = La paz nunca fue una opción
-            var asciiCode = "076 097 032 112 097 122 032 110 117 110 099 097 032 102 117 101 032 117 110 097 032 111 112 099 105 195 179 110";
+            var asciiCode = "076097032112097122032110117110099097032102117101032117110097032111112099105195179110";
+
 
             Console.WriteLine( string.Format("Ascii code: {0}",asciiCode));
 
@@ -25,7 +26,13 @@ namespace AsciiDecoding
             //1 Hacer split para obtener cada byte en string
             //2 Convertir strig a byte
             //3 Crear array
-            var asciiBytes = asciiCode.Split(' ').Select(valor => Convert.ToByte(valor)).ToArray();
+
+
+            //Hacer split cada 3 caracteres
+            var splitedMessage = Split(asciiCode, 3);
+
+            //Obtener bytes
+            var asciiBytes = splitedMessage.Select(valor => Convert.ToByte(valor)).ToArray();
 
             //Convertir bytes a string con ascci
             var asciiMessage = Encoding.ASCII.GetString(asciiBytes);
@@ -39,6 +46,18 @@ namespace AsciiDecoding
             Console.WriteLine(string.Format("utf8 encoding message: {0}", utf8Message));
 
             Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Divide String en un array, dependiendo del tamaño
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="chunkSize"></param>
+        /// <returns></returns>
+        static IEnumerable<string> Split(string str, int chunkSize)
+        {
+            return Enumerable.Range(0, str.Length / chunkSize)
+                .Select(i => str.Substring(i * chunkSize, chunkSize));
         }
     }
 }
