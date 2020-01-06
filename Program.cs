822763f9e0b1037c6fcf7ee9a286309a54656297
Cesar GONZALEZ <cesar.gonzalez@consulting-for.edenred.com>
2020-01-06 12:34:01 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsciiDecoding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Example();
+        }
+
+
+        private static void Example()
+        {
+            var asciiCode = "076 097 032 112 097 122 032 110 117 110 099 097 032 102 117 101 032 117 110 097 032 111 112 099 105 195 179 110";
+            Console.WriteLine( string.Format("Ascii code: {0}",asciiCode));
+
+            //Mensaje ASCII = La paz nunca fue una opción
+
+            //Convertir string a array de bytes con expresion lambda
+            //1 Hacer split para obtener cada byte en string
+            //2 Convertir strig a byte
+            //3 Crear array
+            var asciiBytes = asciiCode.Split(' ').Select(v => Convert.ToByte(v)).ToArray();
+
+            //Convertir bytes a string con ascci
+            var asciiMessage = Encoding.ASCII.GetString(asciiBytes);
+
+            Console.WriteLine(string.Format("Ascii encoding message: {0}", asciiMessage));
+
+
+            //Convertir bytes a string con utf8
+            var utf8Message = Encoding.UTF8.GetString(asciiBytes);
+
+            Console.WriteLine(string.Format("utf8 encoding message: {0}", utf8Message));
+
+            Console.ReadLine();
+        }
+    }
+}
